3a74d109e7b1e33c87f5add87e45565586e04c52
paul@kul-wsl <paul.ortmann@kuleuven.be>
2020-11-25 15:39:04 +0100

    add dtapy dir
    Former-commit-id: f8cf318d485c2f44ae607b566abe152ce4a7fada [formerly 7ecde02771b7a121c2220d4e312ed873c2df04e4]
    Former-commit-id: ffa7ed919d7c18498184e63735141292cafc2949

---
+++ dtapy/internals/experimental/dict_forward_star.py
@@ -0,0 +1,32 @@
+#  This file is part of the Traffic Assignment Package developed at KU Leuven.
+#  Copyright (c) 2020 Paul Ortmann
+#  License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007, see license.txt
+#  More information at: https://gitlab.mech.kuleuven.be/ITSCreaLab
+#  or contact: ITScrealab@kuleuven.be
+#
+#
+#
+#
+#
+import numba as nb
+import numpy as np
+from collections import OrderedDict
+
+
+@nb.njit
+def make_forward_stars(edge_array, number_of_nodes):
+    forward_star = nb.typed.Dict()
+    star_sizes = np.zeros(number_of_nodes, dtype=nb.int64)
+    nodes = np.arange(number_of_nodes, dtype=nb.int64)
+    for i in nodes:
+        forward_star[i] = np.empty(10,
+                                   dtype=nb.int64)  # In traffic networks nodes will never have more than 10 outgoing edges..
+    for row in edge_array:
+        i, j = row[0], row[1]
+        forward_star[i][star_sizes[i]] = j
+        star_sizes[i] += 1
+    for i in nodes:
+        forward_star[i] = forward_star[i][:star_sizes[i]]
+    return forward_star
+
+
