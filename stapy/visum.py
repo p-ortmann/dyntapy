c424f5c137ac6434edd79fb2f72a7153a362ab64
paul@kul-wsl <paul.ortmann@kuleuven.be>
2020-11-25 12:23:58 +0100

    adding gitignores, renaming in visum.py
    Former-commit-id: 275b37370aef866b6ff6230009d5f3770fb09bbb [formerly 60e7f66a611594a5e9d476439cbd24e919cafc8d]
    Former-commit-id: bb2ebba16790d0dcb36e4aac13c56587699f5f5a

--- stapy/visum.py
+++ stapy/visum.py
@@ -5,8 +5,6 @@
 #  or contact: ITScrealab@kuleuven.be
 #
 #
-import dtapy.internals.jitclasses
-
 try:
     import win32com.client
     import os
@@ -49,13 +47,12 @@ try:
 
 
     def __get_network_data(visum: win32com.client.CDispatch):
-        no = [int(val) for _, val in list(dtapy.internals.jitclasses.Links.GetMultiAttValues('No', True))]
-        from_node = [int(val) for _, val in list(
-            dtapy.internals.jitclasses.Links.GetMultiAttValues('FromNodeNo', True))]
-        to_node = [int(val) for _, val in list(dtapy.internals.jitclasses.Links.GetMultiAttValues('ToNodeNo', True))]
-        caps = [val for _, val in list(dtapy.internals.jitclasses.Links.GetMultiAttValues('CapPrt', True))]
-        lengths = [val for _, val in list(dtapy.internals.jitclasses.Links.GetMultiAttValues('Length', True))]
-        maxspeeds = [val for _, val in list(dtapy.internals.jitclasses.Links.GetMultiAttValues('V0PrT', True))]
+        no = [int(val) for _, val in list(visum.Net.Links.GetMultiAttValues('No', True))]
+        from_node = [int(val) for _, val in list(visum.Net.Links.GetMultiAttValues('FromNodeNo', True))]
+        to_node = [int(val) for _, val in list(visum.Net.Links.GetMultiAttValues('ToNodeNo', True))]
+        caps = [val for _, val in list(visum.Net.Links.GetMultiAttValues('CapPrt', True))]
+        lengths = [val for _, val in list(visum.Net.Links.GetMultiAttValues('Length', True))]
+        maxspeeds = [val for _, val in list(visum.Net.Links.GetMultiAttValues('V0PrT', True))]
         link_data = tuple([(u, v, {'capacity': cap,
                                    'from_node_visum': u,
                                    'to_node_visum': v,
@@ -63,9 +60,9 @@ try:
                                    'length': length,
                                    'maxspeed': speed}) for u, v, link_id, cap, length, speed in
                            zip(from_node, to_node, no, caps, lengths, maxspeeds)])
-        node_ids = [int(val) for _, val in list(dtapy.internals.jitclasses.Nodes.GetMultiAttValues('No', True))]
-        node_xs_raw = [val for _, val in list(dtapy.internals.jitclasses.Nodes.GetMultiAttValues('XCoord', True))]
-        node_ys_raw = [val for _, val in list(dtapy.internals.jitclasses.Nodes.GetMultiAttValues('YCoord', True))]
+        node_ids = [int(val) for _, val in list(visum.Net.Nodes.GetMultiAttValues('No', True))]
+        node_xs_raw = [val for _, val in list(visum.Net.Nodes.GetMultiAttValues('XCoord', True))]
+        node_ys_raw = [val for _, val in list(visum.Net.Nodes.GetMultiAttValues('YCoord', True))]
         in_proj, out_proj = Proj('epsg:31370'), Proj('epsg:4326')  # from belge_lambert to lat lon (epsg4326)
         node_xs, node_ys = transform(in_proj, out_proj, node_xs_raw, node_ys_raw)
         node_data = tuple([(u, {'lon': x, 'lat': y}) for u, x, y in zip(node_ids, node_xs, node_ys)])
