6437f8bda88e8ceb91c65e301acb31f5bc9e4d84
paul@kul-wsl <paul.ortmann@kuleuven.be>
2020-11-29 20:03:55 +0100

    mermaid class diagramm
    Former-commit-id: dd666140335846d93444ef36654d2a3adcdcd2af [formerly 00c179467e0bf430681c096aef1005700a888960]
    Former-commit-id: 4af1ee30f79272c1ee6d2714711a09f4a00cf62c

--- dtapy/core/jitclasses.py
+++ dtapy/core/jitclasses.py
@@ -14,6 +14,7 @@ from numba.typed.typedlist import List
 from heapq import heappush, heappop
 from datastructures.csr import construct_sparse_link_matrix
 from numba.experimental import jitclass
+from dtapy.parameters import link_capacity_id, registered_events
 
 csr_type = CSRMatrix.class_type.instance_type
 
@@ -44,6 +45,30 @@ class Links(object):
         self.receiving_flow = receiving_flow
         self.forward  # csr linkxlink row is outgoing turns
         self.backward  # csr incoming turns
+        self.event_changes  # csr link_id x attribute_ids = val, checks necessary if future values are extracted
+
+    if registered_events[link_capacity_id]:
+        def get_capacity(self, link_id, time=0):  # timing access and overhead through the sparse check,
+            # possibly different approach, depends on whether full array is needed for the 'time'
+            # or only a single entry
+            # alternative: add a check on whether event queue exists for this attribute (possibly hard, at compile time the object
+            # doesn't exist yet ...# - can functions be compiled differently based on this?
+            # TODO: test this design on compatibility with numba
+            if time == 0:
+                return self.capacity[link_id]
+            else:
+                if self.event_changes.get_nnz(link_id) != link_capacity_id:
+                    return self.capacity[link_id]
+                else:
+                    # interpolation ..
+                    return 0
+    else:
+        def get_capacity(self, link_id, time=0): # this will get inlined by the jit compiler - no overhead .
+            return self.capacity[link_id]
+    def get_var(self, link_id, attribute, time=0):
+
+    # TODO: explore and enforce use of @property decorator for those properties that are allowed public access
+    # TODO: name mangle properties that should remain private
 
 
 spec_results = [('',), ]
@@ -55,6 +80,7 @@ class Results(object):
         self.turning_fractions
         self.flows
         self.path_set  # list of all used paths by od pair
+        self.controller_strategy
 
 
 spec_node = [('forward', csr_type),
@@ -76,6 +102,8 @@ class Nodes(object):
         self.position_first_in
         self.position_first_out
         self.nbtf
+        self.event_changes
+        #nodes.forward.getrow(node_id)=link_ids
 
 
 spec_turn = [('fractions', csr_type)]
@@ -88,6 +116,7 @@ class Turns(object):
         self.fractions = fractions  # node x turn_ids
         self.db_restrictions = db_restrictions  # destinations x turns,
         self.receiving_flow = receiving_flow
+        self.event_changes
 
 
 spec_demand = {'links': Links.class_type.instance_type}
@@ -133,7 +162,7 @@ class DynamicEvent(object):
     # TODO: think about if there should be DynamicArrayEvents and DynamicCSREvents, can't both be handled by the same class
     # dynamically closing a turn is not possible with this design (maybe through capcity?), i guess that is not problematic
     def __init__(self, name, control_array):
-        self.event_queue = List.empty_list(tup_type)
+        self.__event_queue = List.empty_list(tup_type)
         self.control_array = control_array
 
     def add_event(self, time, obj_index, val):
@@ -169,7 +198,20 @@ class Network(object):
         self.tot_links = tot_links
         self.tot_nodes = tot_nodes
         self.tot_destinations = tot_destinations
+        self.initial_state_based_on_control
         # TODO: add lookup tables for name to index
+
+    def update_event_changes(self, dt, T):
+        # dt current time step
+        # T time horizon
+        # sets event changes in links turns nodes in accordance with what is registered in dynamic events and
+        # static events for that time period
+        # reduces overhead induced by these event mechanisms ..
+        # restricts controller to register changes in time horizon steps, is this
+        pass
+    def get_controller_response(self,dt,T, Results:Results):
+        pass
+
 
 
 def create_dynamic_event_cls(type):
@@ -178,3 +220,9 @@ def create_dynamic_event_cls(type):
     # rebuild the class below and return it ..
 
     return DynamicEvent
+
+# how to handle dynamic changes, proposal: 3 column array with time stamp and value, temporary structutre for computations
+# that reach into the past with +- k * dt(travel time on longest link), rewritten for each dt in the simulation
+# alternative:
+# register sparse matrix for each object, with obj_id x attribute_id=val as change in analysis TP, retrieval for specific time has
+# overhead of checking the sparse structure ..
---
+++ dtapy/core/technical.md
@@ -0,0 +1,47 @@
+### Class Structure and Design Choices
+classDiagram
+    Network -- Links
+    Network -- Nodes
+    Network -- Turns
+    Network : Links
+    Network : Nodes
+    Network : Turns
+    Network : List <DynamicEvent> dynamic_events
+    Network : List <CSRMatrix> static_events
+    Network : process_events(time)
+    Network : get_controller_response(time, results)
+    Links: Array1D <float64> capacity
+    Links: Array1D <float64> kJam
+    Links: Array1D <float64> sending_flow
+    Links: Array1D <float64> receiving_flow
+    Links: Array1D <float64> length
+    Links: Array1D <int64> from_node
+    Links: Array1D <int64> to_node
+    Links: Array3D <float64> travel_time
+    Links: Array3D <float64> flows
+    Links: CSRMatrix <int64> forward
+    Links: CSRMatrix <int64> backward
+    Links: CSRMatrix <float64> event_changes
+    Links: ...
+    class DynamicEvent{
+    List <Tuple> event_queue
+    Array1D <float64> controlled
+    add_event(time, obj_index, val)
+    get_next_event()
+    pop_next_event()
+    }
+    class Nodes{
+    CSRMatrix <int64> forward
+    CSRMatrix <int64> backward
+    CSRMatrix <float64> event_changes
+    ...
+    }
+    class Turns{
+    CSRMatrix <float64> fractions
+    CSRMatrix <bool> db_restrictions
+    CSRMatrix <float64> event_changes
+    ...
+    }
+     
+
+
