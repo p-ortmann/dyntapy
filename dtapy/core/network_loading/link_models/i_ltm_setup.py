float single precision bug.
Former-commit-id: e98663fff26dee15f903a8c65c1eaccd709a968c [formerly 1428ecfbe7a11edba53d5ce85913b11a99f5bb93]
Former-commit-id: eee7e5f0e62445d5397f9d82bd5cee9154f5dc2a

dtapy/core/network_loading/link_models/i_ltm_setup.py
@@ -90,14 +90,14 @@ def i_ltm_setup(network: Network, time: SimulationTime, dynamic_demand: Internal
     tot_links = network.tot_links
     tot_nodes = network.tot_nodes
     tot_turns = network.tot_turns
-    costs = np.empty((tot_links, tot_time_steps),
+    costs = np.zeros((tot_links, tot_time_steps),
                      dtype=np.float32)  # order of arguments is changed here, for route choice
     # iterate over multiple time steps for a single link ..
     t0 = length / v0
     for t in range(tot_time_steps):
         costs[:, t] = t0
     cvn_up = np.zeros((tot_time_steps, tot_links, tot_destinations), dtype=np.float32)
-    cvn_down = np.empty_like(cvn_up)
+    cvn_down = np.zeros_like(cvn_up)
     con_up = np.full((tot_time_steps, tot_links), False, dtype=np.bool_)
     con_down = np.full((tot_time_steps, tot_links), False, dtype=np.bool_)
     nodes_2_update = np.full((tot_time_steps, tot_nodes), False, dtype=np.bool_)

dtapy/visualization.py
@@ -117,9 +117,13 @@ def show_assignment(g: nx.DiGraph, flows, costs, time: SimulationTime, link_kwar
 
     """
     for key, item in zip(link_kwargs.keys(), link_kwargs.values()):
-        link_kwargs[key] = item.tolist()
+        if type(link_kwargs[key])==np.ndarray:
+            if item.dtype == np.float32:
+                link_kwargs[key] = item.astype(np.float64).round(2).tolist()
     for key, item in zip(node_kwargs.keys(), node_kwargs.values()):
-        node_kwargs[key] = item.tolist()
+            if type(node_kwargs[key]) == np.ndarray:
+                if item.dtype == np.float32:
+                    node_kwargs[key] = item.astype(np.float64).round(2).tolist()
 
     plot = figure(plot_height=plot_size,
                   plot_width=plot_size, x_axis_type="mercator", y_axis_type="mercator",
@@ -163,18 +167,19 @@ def show_assignment(g: nx.DiGraph, flows, costs, time: SimulationTime, link_kwar
     edge_renderer = plot.add_glyph(edge_source,
                                    glyph=Patches(xs='x', ys='y', fill_color='color', line_color=traffic_cm[0],
                                                  line_alpha=0.8))
-    edge_tooltips = [(item, f'@{item}') for item in parameters.visualization.edge_keys if
+    edge_tooltips = [(item, f'@{item}') for item in parameters.visualization.edge_keys + list(link_kwargs)
+                                                                                              if
                      item != 'flow']
-    link_kwargs_tooltips = [(item, '@' + str(item)) + '{(0.00)}' for item in list(link_kwargs.keys())]
-    edge_tooltips = edge_tooltips + link_kwargs_tooltips
+    #link_kwargs_tooltips = [(item, '@' + str(item) + '{(0.00)}') for item in list(link_kwargs.keys())]
+    #edge_tooltips = edge_tooltips + link_kwargs_tooltips
     edge_tooltips.append(('flow', '@flow{(0.0)}'))
     node_renderer = plot.add_glyph(node_source,
                                    glyph=Circle(x='x', y='y', size=max_width_bokeh,
                                                 line_color="black",
                                                 line_width=max_width_bokeh / 5))
-    node_tooltips = [(item, f'@{item}') for item in parameters.visualization.node_keys ]
-    node_kwargs_tooltips = [(item, '@' + str(item)) + '{(0.00)}' for item in list(node_kwargs.keys())]
-    node_tooltips= node_tooltips+node_kwargs_tooltips
+    node_tooltips = [(item, f'@{item}') for item in parameters.visualization.node_keys + list(node_kwargs.keys()) ]
+    #node_kwargs_tooltips = [(item, '@' + str(item) + '{(0.00)}') for item in list(node_kwargs.keys())]
+    #node_tooltips= node_tooltips+node_kwargs_tooltips
 
     edge_hover = HoverTool(show_arrow=False, tooltips=edge_tooltips, renderers=[edge_renderer])
     node_hover = HoverTool(show_arrow=False, tooltips=node_tooltips, renderers=[node_renderer])
@@ -195,8 +200,6 @@ def show_assignment(g: nx.DiGraph, flows, costs, time: SimulationTime, link_kwar
     plot.add_tools(node_hover, edge_hover, nodetaptool)
 
     # Set up callbacks
-    link_kwargs = {key: list(val[0]) for key, val in
-                   zip(link_kwargs.keys(), link_kwargs.values())}
     link_call_back = CustomJS(
         args=dict(source=edge_source, all_x=all_x, all_y=all_y, flows=flows, costs=costs, all_colors=all_colors,
                   link_kwargs=link_kwargs), code="""

testing_dtapy/testing_visualization.py
@@ -28,7 +28,7 @@ costs = np.random.rand(time.tot_time_steps * g.number_of_edges()).reshape(
     (time.tot_time_steps, g.number_of_edges())) * 200
 convergence = np.arange(1, 0, -0.01)
 link_kwargs = {'other_flows':flows*0.7}
-node_kwargs = {'random_node_property':np.arange(g.number_of_nodes()*time.tot_time_steps* 3).reshape(time.tot_time_steps,g.number_of_nodes(),3) }
+node_kwargs = {'random_node_property':np.random.rand(g.number_of_nodes()*time.tot_time_steps* 3).reshape(time.tot_time_steps,g.number_of_nodes(),3) }
 
 show_assignment(g, flows, costs, time, link_kwargs=link_kwargs,node_kwargs=node_kwargs, convergence=convergence)
 show_network(g)