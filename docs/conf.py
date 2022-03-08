acd097b3a670c3abebe5fe4e3fca703d4ee39d20
Paul Ortmann <paul.ortmann@kuleuven.be>
2022-03-08 09:57:56 +0100

    autodoc mock imports
    Former-commit-id: 4acf6df913b84991865ae09e84cc47c06c478bb4 [formerly 00b812b08379afa28b002292919f390caf449087]
    Former-commit-id: d81717894a1947abebdf61b5f5b3718f3406bfeb

--- docs/conf.py
+++ docs/conf.py
@@ -24,16 +24,24 @@ author = 'Paul Ortmann'
 # The full version, including alpha/beta/rc tags
 release = '0.2.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',
+              ]
 
-napoleon_google_docstring = False
+napoleon_google_docstring = False  # using numpydoc via napoleon rather than directly
+# numpydoc itself is rather strict
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
+
+# adding mock imports needed for any machine to build the docs for dyntapy using autodoc
+autodoc_mock_imports = ['networkx', 'osmnx', 'shapely', 'numba', 'bokeh', 'numpy',
+                        'scipy', 'geojson', 'pandas', 'geopandas']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -41,7 +49,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -54,7 +61,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-# napoleon
-napoleon_use_ivar=True
-napoleon_use_rtype=False
-napoleon_use_param=False
